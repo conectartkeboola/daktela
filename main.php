69497b1712cc92b3a57a3ce4251dc16390c3db6d
Jan Novák <jan.novak@conectart.cz>
2016-10-05 12:40:53 +0000

    main.php - pro více instancí Daktela (opraveny chyby v zápisu výst. dat)

--- main.php
+++ main.php
@@ -5,6 +5,7 @@ require_once "vendor/autoload.php";
 $dataDir    = getenv("KBC_DATADIR").DIRECTORY_SEPARATOR;
 $configFile = $dataDir."config.json";
 $config     = json_decode(file_get_contents($configFile), true);
+//$konstanta = $config["parameters"]["konstanta"];
 
 // definice instancí Daktela
 $instances  = array(1,2);   // ID instancí Daktela
@@ -31,9 +32,9 @@ foreach ($instances as $idInstance) {                           // procházení 
     foreach (${"in_records_".$idInstance} as $rowNum => $row) { // ${"in_records_".$idInstance} ... $in_records_1, $in_records_2,...
         if ($rowNum == 0) {                                     // hlavička tabulky 'records'
             $colsNum = count($row);                             // počet sloupců tabulky 'records'
-            for ($i = $startId[$idInstance]; $i < $colsNum; $i++) {
+            for ($i = $startId[$idInstance -1]; $i < $colsNum; $i++) {
                 $out_fields -> writeRow([
-                    $i - $startId + 1,                          // idfield = 1,2,3,...
+                    $i - $startId[$idInstance -1] + 1,          // idfield = 1,2,3,...
                     $row[$i],                                   // názvy formulářových polí
                     $idInstance
                 ]);
@@ -44,7 +45,7 @@ foreach ($instances as $idInstance) {                           // procházení 
                 $out_fieldValues -> writeRow([                  // formulářové pole má vyplněnou hodnotu  
                     $idFieldValue,                              // uměle vytvořený inkrementální index (1,2,3,...)
                     $row[0],                                    // idrecord
-                    $i - $startId + 1,                          // idfield = 1,2,3,...
+                    $i - $startId[$idInstance -1] + 1,          // idfield = 1,2,3,...
                     $row[$i]                                    // value (hodnota ve formulářovém poli)
                 ]);
                 $idFieldValue++;
